b79571953f6894aa082c1374bd67dc400f711cf6
Nguyen Phuong Nam <phuongnam.nguyen@ogilvy.com>
2020-04-20 00:07:45 +0700

    Updates

---
+++ precache-manifest.83e3163d27d15d7009966b32f34ff790.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56ef86418dd5fb6ab65ad1456753e916",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "dbe1cbacb2a4a18b8cf2",
+    "url": "/monsters-rolodex/static/css/main.aa7e9562.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dbe1cbacb2a4a18b8cf2",
+    "url": "/monsters-rolodex/static/js/main.e69d44dc.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
